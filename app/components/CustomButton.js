5a30d4d70459714ff146df8ac27e071185f99578
Moe Lubovac <mlubovac@ubiquia.local>
2016-10-21 10:23:36 -0700

    added additional params

--- app/components/CustomButton.js
+++ app/components/CustomButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const CustomButton = ({ data, metadata }) => (
+const CustomButton = ({ data, rowData, metadata }) => (
     <button
         className="btn btn-secondary" 
-        onClick={metadata.customComponentMetadata.onClick.bind(null, data)}
+        onClick={metadata.customComponentMetadata.onClick.bind(null, data, rowData, metadata)}
     >
         {metadata.displayName}
     </button>
