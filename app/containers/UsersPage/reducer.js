7c1bc5210abfcbf8b38e7ba5563580ca6ddee669
mlubovac <volimcajke@yahoo.com>
2016-10-20 21:30:53 -0700

    added  remove user case

--- app/containers/UsersPage/reducer.js
+++ app/containers/UsersPage/reducer.js
@@ -6,6 +6,11 @@ const gridReducer = (state = [], action) => {
         case "GET_USERS_SUCCESS":
             return payload;
 
+        case "REMOVE_USER":
+            return Object.assign({}, state, {
+                data: state.data.filter(user => user.guid !== payload.guid)
+            });
+
         default: 
             return state;
     }
